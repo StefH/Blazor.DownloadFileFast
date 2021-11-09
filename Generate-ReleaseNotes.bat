ff888e5fdde14847f9b6c9d54ffaa7c708c00800
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-11-09 20:39:58 +0100

    0.1.0

--- Generate-ReleaseNotes.bat
+++ Generate-ReleaseNotes.bat
@@ -2,4 +2,4 @@ rem https://github.com/StefH/GitHubReleaseNotes
 
 SET version=0.1.0
 
-GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid doc --version %version% --token %GH_TOKEN%
+GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid doc help-wanted --version %version% --token %GH_TOKEN%
---
+++ ReleaseNotes.md
@@ -0,0 +1,3 @@
+# 0.1.0 (09 November 2021)
+- [#2](https://github.com/StefH/Blazor.DownloadFileFast/pull/2) - Add .NET 6.0 [enhancement] contributed by [StefH](https://github.com/StefH)
+
