e9d04b3021741fa8a0f2c2b5abc753ed2dcb8fc3
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-11-21 12:00:02 +0100

    Add .NET 6 example

---
+++ Blazor.DownloadFileFast.Example_NET6/Program.cs
@@ -0,0 +1,13 @@
+using Blazor.DownloadFileFast.Example_NET6;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorDownloadFile();
+
+await builder.Build().RunAsync();
