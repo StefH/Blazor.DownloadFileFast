665b602ad3563d76ef35db07156e086d43b70894
StefH <StefH@users.noreply.github.com>
2024-11-07 07:45:58 +0000

    Deploy to GitHub pages

---
+++ _framework/JavaScript/download.js
@@ -0,0 +1,72 @@
+﻿function BlazorDownloadFile(filename, contentType, content) {
+    // In < .net6.0 Blazor marshall byte[] to a base64 string, so we first need to convert the string (content) to a Uint8Array to create the File
+    const data = typeof(content) === "string" ? base64DecToArr(content) : content;
+
+    // Create the URL
+    const file = new File([data], filename, { type: contentType });
+    const exportUrl = URL.createObjectURL(file);
+
+    // Create the <a> element and click on it
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+
+    // We don't need to keep the url, let's release the memory
+    URL.revokeObjectURL(exportUrl);
+
+    return true;
+}
+
+// Convert a base64 string to a Uint8Array. This is needed to create a blob object from the base64 string.
+// The code comes from: https://developer.mozilla.org/fr/docs/Web/API/WindowBase64/D%C3%A9coder_encoder_en_base64
+function b64ToUint6(nChr) {
+    return nChr > 64 && nChr < 91 ? nChr - 65 : nChr > 96 && nChr < 123 ? nChr - 71 : nChr > 47 && nChr < 58 ? nChr + 4 : nChr === 43 ? 62 : nChr === 47 ? 63 : 0;
+}
+
+function base64DecToArr(sBase64, nBlocksSize) {
+    var
+        sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""),
+        nInLen = sB64Enc.length,
+        nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2,
+        taBytes = new Uint8Array(nOutLen);
+
+    for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+        nMod4 = nInIdx & 3;
+        nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+
+        if (nMod4 === 3 || nInLen - nInIdx === 1) {
+            for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+                taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+            }
+            nUint24 = 0;
+        }
+    }
+    return taBytes;
+}
+
+function BlazorDownloadFileFast(name, contentType, content) {
+    // Convert the parameters to actual JS types
+    const nameStr = BINDING.conv_string(name);
+    const contentTypeStr = BINDING.conv_string(contentType);
+    const contentArray = Blazor.platform.toUint8Array(content);
+
+    // Create the URL
+    const file = new File([contentArray], nameStr, { type: contentTypeStr });
+    const exportUrl = URL.createObjectURL(file);
+
+    // Create the <a> element and click on it
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = nameStr;
+    a.target = "_self";
+    a.click();
+
+    // We don't need to keep the url, let's release the memory
+    URL.revokeObjectURL(exportUrl);
+
+    return true;
+}
