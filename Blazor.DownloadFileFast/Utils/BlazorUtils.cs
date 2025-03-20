933528c335931b113138b8d425f3b2313e2c0915
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2025-03-20 11:22:30 +0100

    Add logic to detect WebAssembly (#13)
    * Add logic to detect WebAssembly
    
    * ,
    
    * 03

---
+++ Blazor.DownloadFileFast/Utils/BlazorUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Blazor.DownloadFileFast.Utils;
+
+internal static class BlazorUtils
+{
+    internal static readonly bool IsWASM;
+
+    static BlazorUtils()
+    {
+        IsWASM = RuntimeInformation.IsOSPlatform(OSPlatform.Create("BROWSER"));
+    }
+}
